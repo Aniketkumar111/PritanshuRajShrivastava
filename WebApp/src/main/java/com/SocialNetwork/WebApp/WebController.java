3134e23c5afa457e45732659cca96c2bd659de92
HP <HP@Aniket.mshome.net>
2019-06-25 13:52:23 +0530

    First commit to the project.

---
+++ WebApp/src/main/java/com/SocialNetwork/WebApp/WebController.java
@@ -0,0 +1,21 @@
+package com.SocialNetwork.WebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index.html";
+		
+	}
+	
+
+	@RequestMapping("/")
+	public String login() {
+		return "Login.html";
+		
+	}
+}
